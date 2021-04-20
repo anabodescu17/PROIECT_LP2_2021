479a5dfc497c45c2957453c20df9e47fce369eb5
EmilGabriel5 <82525560+EmilGabriel5@users.noreply.github.com>
2021-04-20 17:57:07 +0300

    Afisarea descriere

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("salut emi si andreea")
-print("Buna Ana si Andreea")
+import requests
+from bs4 import BeautifulSoup
+URL = 'https://xpage.primariatm.ro/lotusweb.nsf/autorizatiiurbanism.xsp'
+
+my_headers = {"User-Agent":"Opera GX/72.3"}
+
+page = requests.get(URL, headers=my_headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+Descriere= soup.find_all( class_ = 'xspColumnViewEnd') #afiseaza continutul coloanei 4
+for i in Descriere:
+    print(i.text)
