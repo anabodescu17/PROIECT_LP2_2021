8a92a1aae5926c8bc5cc472bac39d8199d53f11e
cerbu <cerbug@yahoo.com>
2021-04-20 20:23:19 +0300

    Afisare coloana 2

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("salut emi si andreea")
-print("Buna Ana si Andreea")
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://xpage.primariatm.ro/lotusweb.nsf/autorizatiiurbanism.xsp'
+
+my_headers = {"User-Agent": "Google Chrome/12.7"}
+
+page = requests.get (URL, headers=my_headers)
+soup = BeautifulSoup (page.content, 'html.parser')
+
+Beneficiar = soup.find_all ('td', style='font-weight:bold')  # afiseaza continutul coloanei 2
+for i in Beneficiar:
+    print (i.text)
