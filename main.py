b7d89afde9c69978c80c429732dbe736d2b0e68c
EmilGabriel5 <82525560+EmilGabriel5@users.noreply.github.com>
2021-05-16 17:43:13 +0300

    Selectare numar strada si nume strada

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas
+import re
+
 
 URL = 'https://xpage.primariatm.ro/lotusweb.nsf/autorizatiiurbanism.xsp'
 my_headers = {"User-Agent": "Mozilla/5.0"}
 page = requests.get(URL, headers=my_headers)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-a = soup.find('tbody')
-b = a.find_all('tr', role="row")
-"""
-print(b[0])
-print(b[0].find(style="font-family:Courier New CE;font-size:10pt").get_text())
-print(b[0].find(style="font-weight:bold").get_text())
-print(b[0].find(class_='xspColumnViewEnd').get_text())
-"""
-Nr_Data = [item.find(style="font-family:Courier New CE;font-size:10pt").text for item in b]
-Beneficiar = [item.find(style="font-weight:bold").text for item in b]
-Descriere = [item.find(class_='xspColumnViewEnd').text for item in b]
+tbody = soup.find("tbody")
+columns = []
+for tr in tbody.find_all('tr'):
+    columns.append([tr.text for tr in tr.find_all("td")])
+columns = list(zip(*columns))
 
-print(Nr_Data)
-print(Beneficiar)
-print(Descriere)
+
+print(columns[0])
+print(columns[1])
+print(columns[2])
+print(columns[3])
+
+numar_strada = []
+nume_strada = []
+for i in columns[2]:
+    numar = re.search(r"(nr.+)", i)
+
+    nume = re.search(r".+?(?=nr)", i)
+    if numar:
+        numar_strada.append(numar.group())
+    if nume:
+        nume_strada.append(nume.group())
+print(nume_strada)
+print(numar_strada)
