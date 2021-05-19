fafbfdbe108150dda119a2fc7d18a853368e26a7
EmilGabriel5 <82525560+EmilGabriel5@users.noreply.github.com>
2021-05-19 11:15:01 +0300

    Update main.py
    Etapa finala

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas
+import re
 
+URL = 'https://xpage.primariatm.ro/lotusweb.nsf/autorizatiiurbanism.xsp'
+my_headers = {"User-Agent": "Mozilla/5.0"}
+page = requests.get(URL, headers=my_headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+tbody = soup.find("tbody")
+columns = []
+for tr in tbody.find_all('tr'):
+    columns.append([tr.text for tr in tr.find_all("td")])
+columns = list(zip(*columns))
+
+print(columns[0])
+print(columns[1])
+print(columns[2])
+print(columns[3])
+
+numar_strada = []
+nume_strada = []
+for i in columns[2]:
+    numar = re.search(r"(nr.+)", i)
+    nume = re.search(r".+?(?=nr)", i)
+    if numar:
+        numar_strada.append(numar.group())
+    if nume:
+        nume_strada.append(nume.group())
+print(nume_strada)
+print(numar_strada)
+
+df = pandas.DataFrame({'Nr./Data AC': columns[0],
+                       'Beneficiar': columns[1],
+                       'Adresa':columns[2],
+                       'Descriere': columns[3]})
+print(df)
+df.to_csv('Autorizatii1.csv', encoding='utf-8-sig')
